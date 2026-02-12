ef3b740d0b3059717646ce5dab1bd6560c92a94f
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-12 18:16:56 +0530

    Feat: Adding new controller uploadDealerSellExcel. &  Refactor: Replacing service name with smart_tyre_dashboard.

--- app/functionality/dashboard.js
+++ app/functionality/dashboard.js
@@ -1,12 +1,14 @@
 const response = require("../utility/api_handler");
 const chartFormatter = require("../utility/chart_formatter");
 const services = require('../services/index');
+const fs = require('fs');
+const XLSX = require('xlsx');
 
 module.exports = {
     dealerInstallationLineChart: (req, res) => {
         const type=req.query.type || "7days";
 
-        return services.dealer.getLineChartDealerInstallations(type).then((data)=>{
+        return services.smart_tyre_dashboard.getLineChartDealerInstallations(type).then((data)=>{
 
             const LineChartData=chartFormatter.lineChart(data);
 
@@ -20,7 +22,7 @@ module.exports = {
     zoneWiseDealerInstallationsPie: (req, res) => {
         const type=req.query.type;
 
-        return services.dealer.zoneWiseDealerInstallation(type).then((data)=>{
+        return services.smart_tyre_dashboard.zoneWiseDealerInstallation(type).then((data)=>{
             return res.json(response.JsonMsg(true, data, "Zone-wise Installations Data", 200));
         }).catch((err)=>{
             console.error(err);
@@ -29,7 +31,7 @@ module.exports = {
     },
 
     dealerInstallationTable: (req, res) => {
-        return services.dealer.dealerInstallation().then((data)=>{
+        return services.smart_tyre_dashboard.dealerInstallation().then((data)=>{
             return res.json(response.JsonMsg(true, data, "Dealer Installations Data", 200));
         }).catch((err)=>{
             console.error(err);
@@ -39,7 +41,7 @@ module.exports = {
 
     top5DealerInstallationTable: (req, res) => {
 
-        return services.dealer.top5DealerInstallation().then((data)=>{
+        return services.smart_tyre_dashboard.top5DealerInstallation().then((data)=>{
             return res.json(response.JsonMsg(true,data, "Dealer Installations Data for top 5 regions", 200));
         }).catch((err)=>{
             console.error(err);
@@ -49,7 +51,7 @@ module.exports = {
 
     top5MakeModelTable: (req, res) => {
 
-        return services.dealer.top5MakeModel().then((data)=>{
+        return services.smart_tyre_dashboard.top5MakeModel().then((data)=>{
             return res.json(response.JsonMsg(true,data, "Dealer Installations Data for top 5 regions", 200));
         }).catch((err)=>{
             console.error(err);
@@ -59,7 +61,7 @@ module.exports = {
 
     top5regionTable: (req, res) => {
 
-        return services.dealer.top5region().then((data)=>{
+        return services.smart_tyre_dashboard.top5region().then((data)=>{
             return res.json(response.JsonMsg(true,data, "Dealer Installations Data for top 5 regions", 200));
         }).catch((err)=>{
             console.error(err);
@@ -69,11 +71,63 @@ module.exports = {
 
     top5ZoneTable: (req, res) => {
 
-        return services.dealer.top5Zone().then((data)=>{
+        return services.smart_tyre_dashboard.top5Zone().then((data)=>{
             return res.json(response.JsonMsg(true,data, "Dealer Installations Data for top 5 zones", 200));
         }).catch((err)=>{
             console.error(err);
             return res.json(response.JsonMsg(false, null , "Failed to fetch data", 500));
         })
+    },
+
+    uploadDealerSellExcel: (req, res) => {
+
+        const deleteFile = (filePath) => {
+            if (filePath && fs.existsSync(filePath)) {
+                fs.unlinkSync(filePath);
+            }
+        };
+
+        if (!req.file) {
+            return res.json(
+                response.JsonMsg(false, null, "No file uploaded", 400)
+            );
+        }
+
+        let rows;
+
+        try {
+            const workbook = XLSX.readFile(req.file.path);
+            const sheet = workbook.Sheets[workbook.SheetNames[0]];
+            rows = XLSX.utils.sheet_to_json(sheet);
+
+            if (!rows.length) {
+                deleteFile(req.file.path);
+                return res.json(
+                    response.JsonMsg(false, null, "Empty file", 400)
+                );
+            }
+        } catch (err) {
+            deleteFile(req.file.path);
+            return res.json(
+                response.JsonMsg(false, null, "Invalid Excel file", 400)
+            );
+        }
+
+        return services.smart_tyre_dashboard.uploadDealerSellExcel(rows)
+            .then(result => {
+
+                deleteFile(req.file.path);
+                return res.json(
+                    response.JsonMsg(true, result.length, "Excel uploaded successfully", 200)
+                );
+            })
+            .catch(err => {
+
+                deleteFile(req.file.path);
+                return res.json(
+                    response.JsonMsg(false, null, err.message, 400)
+                );
+            });
     }
+
 }

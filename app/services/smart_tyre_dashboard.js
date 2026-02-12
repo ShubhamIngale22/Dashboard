30cd12e812fb4ae40c44bddc0b94398f037426d8
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-12 18:14:07 +0530

    Feat: Adding new controller uploadDealerSellExcel.

--- app/services/smart_tyre_dashboard.js
+++ app/services/smart_tyre_dashboard.js
@@ -1,28 +1,27 @@
 const model = require('../models/models');
 const constants = require(`../constant/constant`);
 const moment = require("moment");
-const dealer_query = require("../query-provider/dealer")
-const stringConstant = require("../constant/constant");
+const smart_tyre_dashboard = require("../query-provider/smart_tyre_dashboard");
 const XLSX = require('xlsx');
 const dateHelper=require('../helpers/date_helper');
 
-module.exports={
+module.exports= {
     getLineChartDealerInstallations: (type) => {
         let startDate;
         let format;
 
         if (type === "1year") {
             startDate = moment().subtract(1, "year").toDate();
-            format= "%Y-%m";
+            format = "%Y-%m";
         } else if (type === "30days") {
             startDate = moment().subtract(30, "days").toDate();
-            format= "%m-%d";
-        }else{
+            format = "%m-%d";
+        } else {
             startDate = moment().subtract(7, "day").toDate();
-            format= "%Y-%m-%d";
+            format = "%Y-%m-%d";
         }
 
-        return model.dealerInstallation.aggregate(dealer_query.getLineChartDealerInstallations(startDate, format)).then((data) => {
+        return model.dealerInstallation.aggregate(smart_tyre_dashboard.getLineChartDealerInstallations(startDate, format)).then((data) => {
             return Promise.resolve(data)
         }).catch((err) => {
             return Promise.reject(err);
@@ -35,15 +34,15 @@ module.exports={
 
         if (type === "yearly") {
             startDate = moment().subtract(1, "year").startOf("year").toDate();
-            endDate   = moment().subtract(1, "year").endOf("year").toDate();
+            endDate = moment().subtract(1, "year").endOf("year").toDate();
         } else {
             startDate = moment().subtract(1, "month").startOf("month").toDate();
-            endDate   = moment().subtract(1, "month").endOf("month").toDate();
+            endDate = moment().subtract(1, "month").endOf("month").toDate();
         }
-        return model.dealerInstallation.aggregate(dealer_query.getZoneWiseDealerInstallation(startDate, endDate)).then(result =>{
-            return result ;
-        }).catch(err=>{
-            console.error("ZoneDealerInstallations service error is :",err);
+        return model.dealerInstallation.aggregate(smart_tyre_dashboard.getZoneWiseDealerInstallation(startDate, endDate)).then(result => {
+            return result;
+        }).catch(err => {
+            console.error("ZoneDealerInstallations service error is :", err);
             throw err;
         })
     },
@@ -51,64 +50,64 @@ module.exports={
     dealerInstallation: () => {
 
         const yesterdayStart = moment().subtract(1, "day").startOf("day").toDate();
-        const yesterdayEnd   = moment().subtract(1, "day").endOf("day").toDate();
+        const yesterdayEnd = moment().subtract(1, "day").endOf("day").toDate();
 
         const lastMonthStart = moment().subtract(1, "month").startOf("month").toDate();
-        const lastMonthEnd   = moment().subtract(1, "month").endOf("month").toDate();
+        const lastMonthEnd = moment().subtract(1, "month").endOf("month").toDate();
 
         const lastYearStart = moment().subtract(1, "year").startOf("year").toDate();
-        const lastYearEnd   = moment().subtract(1, "year").endOf("year").toDate();
+        const lastYearEnd = moment().subtract(1, "year").endOf("year").toDate();
 
-        return model.dealerInstallation.aggregate(dealer_query.getDealerInstallation(yesterdayStart,yesterdayEnd,lastMonthStart,lastMonthEnd,lastYearStart,lastYearEnd)).then(result => {
+        return model.dealerInstallation.aggregate(smart_tyre_dashboard.getDealerInstallation(yesterdayStart, yesterdayEnd, lastMonthStart, lastMonthEnd, lastYearStart, lastYearEnd)).then(result => {
             const r = result[0] || {};
             return {
                 yesterday: r.yesterday?.[0]?.count || 0,
                 lastMonth: r.lastMonth?.[0]?.count || 0,
                 lastYear: r.lastYear?.[0]?.count || 0
             }
-        }).catch(err=>{
-            console.error("totalDealerInstallations service error is :",err);
+        }).catch(err => {
+            console.error("totalDealerInstallations service error is :", err);
             throw err;
         })
     },
 
     top5DealerInstallation: () => {
-        return model.dealerInstallation.aggregate(dealer_query.getTop5DealerInstallation()).then(result =>{
+        return model.dealerInstallation.aggregate(smart_tyre_dashboard.getTop5DealerInstallation()).then(result => {
             return result;
-        }).catch(err=>{
-            console.error(" top5DealerInstallations Service error is :",err);
+        }).catch(err => {
+            console.error(" top5DealerInstallations Service error is :", err);
             throw err;
         })
     },
 
     top5MakeModel: () => {
-        return model.dealerInstallation.aggregate(dealer_query.getTop5MakeModel()).then(result =>{
+        return model.dealerInstallation.aggregate(smart_tyre_dashboard.getTop5MakeModel()).then(result => {
             return result;
-        }).catch(err=>{
-            console.error(" top5MakeModel Service error is :",err);
+        }).catch(err => {
+            console.error(" top5MakeModel Service error is :", err);
             throw err;
         })
     },
 
     top5region: () => {
-        return model.dealerInstallation.aggregate(dealer_query.getTop5region()).then(result =>{
-            return result ;
-        }).catch(err=>{
-            console.error("top5regions service error is :",err);
+        return model.dealerInstallation.aggregate(smart_tyre_dashboard.getTop5region()).then(result => {
+            return result;
+        }).catch(err => {
+            console.error("top5regions service error is :", err);
             throw err;
         })
     },
 
     top5Zone: () => {
-        return model.dealerInstallation.aggregate(dealer_query.getTop5zone()).then(result =>{
+        return model.dealerInstallation.aggregate(smart_tyre_dashboard.getTop5zone()).then(result => {
             return result;
-        }).catch(err=>{
-            console.error(" top5Zones Service error is :",err);
+        }).catch(err => {
+            console.error(" top5Zones Service error is :", err);
             throw err;
         })
     },
 
-    uploadDealerSaleExcel:(rows) => {
+    uploadDealerSellExcel: (rows) => {
 
         const cleanedRows = rows.map(row => {
             const newRow = {};
@@ -118,31 +117,23 @@ module.exports={
             return newRow;
         });
         const formattedData = cleanedRows.map(row => ({
-                plant: row.plant?.toString().trim(),
-                billingDate: dateHelper.convertExcelDate(row.billingDate),
-                sensorType: row.sensorType,
-                productCode: row.productCode,
-                productDesc: row.productDesc,
-                quantity: Number(row.Quantity || 0),
-                customerCode: row.customerCode?.toString(),
-                dealerShopName: row.dealerShopName,
-                districtName: row.districtName,
-                stateName: row.stateName,
-                cityName: row.cityName,
-                territoryCode: row.territoryCode?.toString(),
-                territoryName: row.territoryName,
-                zone: row.zone,
-                regionName: row.regionName
-            }));
-        return model.dealerSale.insertMany(formattedData, { ordered: false }).then(result => ({
-                success: true,
-                inserted: result.length
-            })).catch(err => {
-                if (err.code === 11000) {
-                    throw new Error("Duplicate file uploaded");
-                }
-                throw err;
-            });
+            plant: row.plant?.toString().trim(),
+            billingDate: dateHelper.convertExcelDate(row.billingDate),
+            sensorType: row.sensorType,
+            productCode: row.productCode,
+            productDesc: row.productDesc,
+            quantity: Number(row.Quantity || 0),
+            customerCode: row.customerCode?.toString(),
+            dealerShopName: row.dealerShopName,
+            districtName: row.districtName,
+            stateName: row.stateName,
+            cityName: row.cityName,
+            territoryCode: row.territoryCode?.toString(),
+            territoryName: row.territoryName,
+            zone: row.zone,
+            regionName: row.regionName
+        }));
+        return model.dealerSell.insertMany(formattedData, {ordered: false});
     }
 
 }

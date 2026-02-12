748fc971919bc284b63698bdd3007272e988074d
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-12 18:05:19 +0530

    Feat: Creating schema for dealerSells data.

---
+++ app/schema/dealerSell.js
@@ -0,0 +1,28 @@
+/* Created by admin on 12-02-2026 */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sellsBillingSchema = new Schema({
+    plant: {type: String,},
+    billingDate: {type: Date},
+    sensorType: {type: String},
+    productCode: {type: String},
+    productDesc: {type: String},
+    quantity: {type: Number},
+    customerCode: {type: String},
+    dealerShopName: {type: String},
+    districtName: {type: String},
+    stateName: {type: String},
+    cityName: {type: String},
+    territoryCode: {type: String},
+    territoryName: {type: String},
+    zone: {type: String},
+    regionName: {type: String}
+},
+    {
+    timestamps: true,
+    versionKey: false
+});
+
+module.exports = mongoose.model('t_dealerSell', sellsBillingSchema);
+

8d4936f46ebc809a39a73dd5f3db41edbd6c5b77
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-13 14:20:18 +0530

    Feat: Updating old service to handle count of dealer sells for table.

--- app/services/smart_tyre_dashboard.js
+++ app/services/smart_tyre_dashboard.js
@@ -67,7 +67,7 @@ module.exports= {
         })
     },
 
-    dealerInstallation: () => {
+    dealerInstallationsSells: () => {
 
         const yesterdayStart = moment().subtract(1, "day").startOf("day").toDate();
         const yesterdayEnd = moment().subtract(1, "day").endOf("day").toDate();
@@ -78,12 +78,35 @@ module.exports= {
         const lastYearStart = moment().subtract(1, "year").startOf("year").toDate();
         const lastYearEnd = moment().subtract(1, "year").endOf("year").toDate();
 
-        return model.dealerInstallation.aggregate(smart_tyre_dashboard.getDealerInstallation(yesterdayStart, yesterdayEnd, lastMonthStart, lastMonthEnd, lastYearStart, lastYearEnd)).then(result => {
-            const r = result[0] || {};
+        const installationsPromise=model.dealerInstallation.aggregate(
+            smart_tyre_dashboard.getDealerInstallation(
+                yesterdayStart, yesterdayEnd,
+                lastMonthStart, lastMonthEnd,
+                lastYearStart, lastYearEnd)
+        );
+
+        const sellsPromise=model.dealerSell.aggregate(
+            smart_tyre_dashboard.getDealerSells(
+                yesterdayStart, yesterdayEnd,
+                lastMonthStart, lastMonthEnd,
+                lastYearStart, lastYearEnd)
+        );
+
+        return Promise.all([installationsPromise,sellsPromise]).then(([installations,sells]) => {
+            const installData = installations?.[0] ?? {};
+            const sellData = sells?.[0] ?? {};
+
             return {
-                yesterday: r.yesterday?.[0]?.count || 0,
-                lastMonth: r.lastMonth?.[0]?.count || 0,
-                lastYear: r.lastYear?.[0]?.count || 0
+                Installations:{
+                    yesterday: installData.yesterday?.[0]?.count ?? 0,
+                    lastMonth: installData.lastMonth?.[0]?.count ?? 0,
+                    lastYear: installData.lastYear?.[0]?.count ?? 0
+                },
+                sells:{
+                    yesterday: sellData.yesterday?.[0]?.count ?? 0,
+                    lastMonth: sellData.lastMonth?.[0]?.count ?? 0,
+                    lastYear: sellData.lastYear?.[0]?.count ?? 0
+                }
             }
         }).catch(err => {
             console.error("totalDealerInstallations service error is :", err);

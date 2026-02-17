4cc5e7e65629a4746679e59acdcd2c305964b0f8
Shubham Ingale <shubhingale23052004@gmail.com>
2026-02-17 10:51:30 +0530

    Feat- Creating object function for the financial year.

--- app/helpers/date_helper.js
+++ app/helpers/date_helper.js
@@ -1,4 +1,5 @@
 const moment=require('moment-timezone');
+const string_decoder = require("string_decoder");
 
 const TIMEZONE="Asia/Kolkata";
 
@@ -31,6 +32,27 @@ module.exports={
             return new Date(`${parts[2]}-${parts[1]}-${parts[0]}`);
         }
         return new Date(date);
-    }
+    },
+
+    fyYearStart:()=>{
+        let startDate;
+        const financialYearStart=moment().month() < 3
+            ? moment().year() - 1
+            : moment().year();
+
+        startDate=moment(`${financialYearStart}-04-01`).startOf("day").toDate();
+        return startDate;
+    },
+
+    fyYearLabel:()=>{
+        const financialYearStart=moment().month() < 3
+            ? moment().year() - 1
+            : moment().year();
+
+        const financialYearEnd=financialYearStart+1;
+
+        return `FY ${String(financialYearStart)}-${String(financialYearEnd).slice(2)}`;
+    },
+
 
 };

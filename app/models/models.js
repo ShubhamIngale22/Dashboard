96884ec1aa04dc044a2bb6fe42612990ce319139
Shubham Ingale <shubhingale23052004@gmail.com>
2026-03-06 10:56:00 +0530

    Feat: Updating models for new user schema.

--- app/models/models.js
+++ app/models/models.js
@@ -1,6 +1,7 @@
 module.exports = {
     dealerInstallation: require('../schema/dealerInstallation'),
-    dealerSell:require("../schema/dealerSell"),
+    dealerSell: require("../schema/dealerSell"),
     dealerInstallationCount: require('../schema/dealerInstallationCount'),
-    dealerInstallationZoneCount: require('../schema/dealerInstallationZoneCount')
+    dealerInstallationZoneCount: require('../schema/dealerInstallationZoneCount'),
+    user: require("../schema/user")
 };

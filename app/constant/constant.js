3907d2192af2faa96afa1cfcd50e2fb354bbf4c5
Shubham Ingale <shubhingale23052004@gmail.com>
2026-03-06 10:54:14 +0530

    Feat: Adding new constant for the JWT.

--- app/constant/constant.js
+++ app/constant/constant.js
@@ -3,6 +3,8 @@ const dbURL = {    "database":  process.env.DB_URL || "mongodb://127.0.0.1:27017
 const PORT = {
     "port": process.env.PORT || 5000
 }
+const JWT_SECRET = process.env.JWT_SECRET || "9f3a4c8b1d0e7f5a9c2b6d8e3a1f4s5f7g3j5d6a0c8f2b1e4d9a7c5f3b8e6";
+const JWT_EXPIRE = process.env.JWT_EXPIRE || "1d";
 const FILE_UPLOAD = {
     uploadPath:"uploads/"
 };
@@ -10,6 +12,8 @@ const FILE_UPLOAD = {
 module.exports = {
     'dbURL': dbURL,
     'PORT': PORT,
-    'FILE_UPLOAD':FILE_UPLOAD
+    'FILE_UPLOAD':FILE_UPLOAD,
+    'JWT_SECRET':JWT_SECRET,
+    'JWT_EXPIRE':JWT_EXPIRE
 };
 
